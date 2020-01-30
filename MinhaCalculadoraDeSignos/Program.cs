c0adc2bbc55d533a089826f6ad23179f472ff263
Gerson Bueno <gersonpereirabueno@hotmail.com>
2020-01-30 03:34:30 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaCalculadoraDeSignos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MinhaCalculadoraDeSignos
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            try
+            {
+                Console.WriteLine("Em que mês você nasceu?");
+                string str1 = Console.ReadLine();
+                if (Convert.ToInt32(str1) > 12)
+                    Console.WriteLine("Digite mês válido");
+                string str2;
+                while (true)
+                {
+                    Console.WriteLine("Em que dia você nasceu?");
+                    str2 = Console.ReadLine();
+                    if (Convert.ToInt32(str2) > 31)
+                        Console.WriteLine("Digite dia válido");
+                    else if (Convert.ToInt32(str1) == 2 && Convert.ToInt32(str2) > 29)
+                        Console.WriteLine("Digite dia válido para esse mês");
+                    else
+                        break;
+                }
+                num1 = Convert.ToInt32(str2);
+                num2 = Convert.ToInt32(str1);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+            Signo signo = new InterpretadorSigno().Interpretar(Convert.ToInt32(num2), Convert.ToInt32(num1));
+            if (signo != null)
+                Console.WriteLine("Seu signo é " + signo.nome + " " + signo.caracteristicas);
+            else
+                Console.WriteLine("Não foi possível interpretar o seu signo. Tente novamente");
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ MinhaCalculadoraDeSignos/Signo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinhaCalculadoraDeSignos
+{
+    class Signo
+    {
+        public string caracteristicas;
+        public int diaFim;
+        public int diaInicio;
+        public int mesFim;
+        public int mesInicio;
+        public string nome;
+    }
+}
